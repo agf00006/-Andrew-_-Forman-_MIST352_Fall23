830a84d0ab2d960bc11c3d82ef6c416fe7f67d45
andrewforman <andrewforman@localhost>
2023-08-24 12:33:07 -0400

    Added GoodbyeWorld project to repo. I am feeling sad.

---
+++ GoodbyeWorld/src/GoodbyeWorld.java
@@ -0,0 +1,24 @@
+/** Author: Andy Forman
+ * Date: 8/24/2023
+ * This is a trial file
+ */
+
+/**
+ * 
+ */
+public class GoodbyeWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Print out a message to users
+System.out.println("This is a message to display to users");
+
+//Print out another message to users
+System.out.println("This is a second message to display to users");
+	}
+
+}
