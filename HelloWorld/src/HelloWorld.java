c05b6792378e8dfe8ee2c4dd6ea389bc67dfc4c8
andrewforman <andrewforman@localhost>
2023-09-06 22:41:40 -0400

    Add HelloWorld to GitHub (unlikely to work)

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+		System.out.println("Welcome to MIST352");
+
+	}
+
+}
