20ddb4b1b2d262e29e6a9aadf360518216a22fec
andrewforman <andrewforman@localhost>
2023-09-10 17:16:42 -0400

    Submitted HW#1

---
+++ Homeworks/HW1A/src/HW1.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ *  Author: Andy Forman
+ *  Class: MIST352-Fall23
+ *  HW#1
+ *  Greet user, calculate 10 + 5.5 and provide an output of the sum
+ */
+//Name of the class should reflect the class name
+public class HW1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Missing semicolon
+				System.out.println("Hi");
+				//int changed to double to match num2
+				double num1 = 10;
+				double num2 = 5.5;
+				//deleted unnecessary line
+				//int changed to double to allow calculation
+				double sum = num1 + num2;
+				
+				System.out.println("Sum: " + sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
